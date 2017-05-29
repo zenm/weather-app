834ec195f05cfce7899105db25213c6c5dd231b8
Lino <linojr@gmail.com>
2017-05-29 15:00:16 -0500

    added get weather function

--- resources/main.js
+++ resources/main.js
@@ -4,17 +4,18 @@ var userLatitude;
 var userLongitude;
 
 function success (position) {
-	// position as an object
-	var loc = position.coords;
-  console.log("latitude: " + loc.latitude);
-	console.log("longitude: " + loc.longitude);
-}
+  var loc = position.coords;
+  userLatitude = loc.latitude;
+  userLongitude = loc.longitude;
+  getWeather();
+};
+
 
 function error () {
 	//doesn't look like it's supported
   console.log("doesn't look supported.")
 }
-navigator.geolocation.getCurrentPosition(success, error);
+
 
 //get weather
 var weatherAPIUrl = "https://api.darksky.net/forecast/"; //https://api.darksky.net/forecast/[key]/[latitude],[longitude]
@@ -22,19 +23,25 @@ var myAPIKey = "a5873da77421eb5e70604462e2ac3e77";
 
 
 $(document).ready(function(){
-  getWeather();
+  navigator.geolocation.getCurrentPosition(success, error);
+
 });
 
 
 function getWeather() {
+  console.log(userLatitude);
+  console.log(userLongitude);
   $.ajax({
-    url : weatherAPIUrl + "/" + myAPIKey + "/" + userLatitude + "," + userLongitude,
+    url : weatherAPIUrl + "/" + myAPIKey + "/" + userLatitude + "," + userLongitude ,
+    // beforeSend: function(request) {
+    //   request.setRequestHeader("Access-Control-Allow-Origin", "*");
+    // },
     method : "GET",
     crossDomain : true,
     dataType: "jsonp",
+    jsonp : "callback",
     success : function(data){
-      console.log(data.currently.summary);
-      console.log(data.currently.temperature);
+      alert(data.currently.summary);
     }
 
   });
