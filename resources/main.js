42ec7a2d71a2a35be2d147987f081f07e2a6e624
Lino <linojr@gmail.com>
2017-05-30 14:51:01 -0500

    Changed APIs to use openweather

--- resources/main.js
+++ resources/main.js
@@ -12,14 +12,18 @@ function success (position) {
 
 
 function error () {
-	//doesn't look like it's supported
-  console.log("doesn't look supported.")
+  console.log("Can't get location. It doesn't look supported.")
 }
 
 
 //get weather
-var weatherAPIUrl = "https://api.darksky.net/forecast/"; //https://api.darksky.net/forecast/[key]/[latitude],[longitude]
-var myAPIKey = "a5873da77421eb5e70604462e2ac3e77";
+// var weatherAPIUrl = "https://api.darksky.net/forecast/"; //https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+// var myAPIKey = "a5873da77421eb5e70604462e2ac3e77";
+
+
+// open weather API: https://openweathermap.org/current
+var weatherAPIUrl = "api.openweathermap.org/data/2.5/weather?lat=41&lon=-87&APPID=7a93cab57a24c21b6ae714cc5e3635e1";
+var myAPIKey = "7a93cab57a24c21b6ae714cc5e3635e1";
 
 
 $(document).ready(function(){
@@ -32,16 +36,21 @@ function getWeather() {
   console.log(userLatitude);
   console.log(userLongitude);
   $.ajax({
-    url : weatherAPIUrl + "/" + myAPIKey + "/" + userLatitude + "," + userLongitude ,
+    url : weatherAPIUrl,
+    // weatherAPIUrl +'lat=' + userLatitude + '&lon=' + userLongitude +'&APPID=' + myAPIKey,
     // beforeSend: function(request) {
-    //   request.setRequestHeader("Access-Control-Allow-Origin", "*");
+    //   request.setRequestHeader(/*"Access-Control-Allow-Origin", "*"*/"Access-Control-Allow-Headers", "x-requested-with" );
     // },
-    method : "GET",
+    // data : {
+    //   lat : userLatitude,
+    //   lon : userLongitude
+    // },
+    method : 'GET',
     crossDomain : true,
-    dataType: "jsonp",
-    jsonp : "callback",
+    dataType: 'jsonp',
     success : function(data){
-      alert(data.currently.summary);
+      console.log(data.main.temp);
+      alert("It worked");
     }
 
   });
