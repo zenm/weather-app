a8d5783c08cc9eaa99d29abce4824017b17df37b
Lino <linojr@gmail.com>
2017-05-31 14:08:54 -0500

    Showing data from API on page

--- resources/main.js
+++ resources/main.js
@@ -16,41 +16,67 @@ function error() {
 
 // weather API Url
 var weatherAPIUrl = "http://api.openweathermap.org/data/2.5/weather";
-var myAPIKey = "7a93cab57a24c21b6ae714cc5e3635e1";
+var myAPIKey = "cce820a8ced713406cddcbdbc688eec5";
 
 
 $(document).ready(function() {
   navigator.geolocation.getCurrentPosition(success, error);
+
 });
 
-
+var tempInImperial;
+var currentCondition;
+var currentConditionIcon;
+var yourCity;
 function getWeather() {
   console.log(userLatitude);
   console.log(userLongitude);
   $.ajax({
     url : weatherAPIUrl,
     data : {
-      lat : userLatitude,
-      lon : userLongitude,
-      units : `imperial`,
-      "APPID" : myAPIKey
+      lat : userLatitude, //41
+      lon : userLongitude, //-82
+      units : "imperial",
+      APPID : myAPIKey
     },
-    method : 'GET',
+    method : "GET",
     crossDomain : true,
-    dataType: 'jsonp',
+    dataType: "jsonp",
     success : function(data){
-      console.log(data.main.temp);
-      // convert metric to imperial
 
+      // convert metric to imperial
+      tempInImperial = Math.round(data.main.temp);
+      currentCondition = data.weather[0].main;
+      currentConditionIcon = data.weather[0].icon;
+      yourCity = data.name;
+      console.log(tempInImperial);
+      console.log(currentCondition);
+      showCurrentWeather();
     }
   });
 }
 
-// convert metrics to imperial and vice versa
-function convertToImperial() {
+// convert metric to imperial and vice versa
 
+$("#imperial-metric-button").on("click", function(){
+  changeToMetricImperial();
+});
+
+function changeToMetricImperial() {
+    $(".f-or-c").text(function(){
+      return this == "Farenheit"? "Celsius":"Farenheit";
+    });
 }
 
-function convertToMetric() {
+// put data on page
+function showCurrentWeather(){
+  if(tempInImperial){
 
+    $("#temperature").text(tempInImperial);
+    $("#current-condition-desc").text(currentCondition);
+    var $imageURL = "http://openweathermap.org/img/w/";
+    //example icon url `http://openweathermap.org/img/w/10d.png`
+    $("#current-condition-image").prepend("<img src=\""+$imageURL+ currentConditionIcon +".png\" alt=\""+ currentCondition + "\">");
+    $("#user-location").text(yourCity);
+  }
 }
