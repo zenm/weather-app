501331a204b43a4d091e2e96b6fb69361d6ea6e2
Lino <linojr@gmail.com>
2017-05-30 22:48:27 -0500

    Added http protocol to API URL

--- resources/main.js
+++ resources/main.js
@@ -3,32 +3,24 @@
 var userLatitude;
 var userLongitude;
 
-function success (position) {
+function success(position) {
   var loc = position.coords;
   userLatitude = loc.latitude;
   userLongitude = loc.longitude;
   getWeather();
 };
 
-
-function error () {
+function error() {
   console.log("Can't get location. It doesn't look supported.")
 }
 
-
-//get weather
-// var weatherAPIUrl = "https://api.darksky.net/forecast/"; //https://api.darksky.net/forecast/[key]/[latitude],[longitude]
-// var myAPIKey = "a5873da77421eb5e70604462e2ac3e77";
-
-
-// open weather API: https://openweathermap.org/current
-var weatherAPIUrl = "api.openweathermap.org/data/2.5/weather?lat=41&lon=-87&APPID=7a93cab57a24c21b6ae714cc5e3635e1";
+// weather API Url
+var weatherAPIUrl = "http://api.openweathermap.org/data/2.5/weather";
 var myAPIKey = "7a93cab57a24c21b6ae714cc5e3635e1";
 
 
-$(document).ready(function(){
+$(document).ready(function() {
   navigator.geolocation.getCurrentPosition(success, error);
-
 });
 
 
@@ -37,21 +29,28 @@ function getWeather() {
   console.log(userLongitude);
   $.ajax({
     url : weatherAPIUrl,
-    // weatherAPIUrl +'lat=' + userLatitude + '&lon=' + userLongitude +'&APPID=' + myAPIKey,
-    // beforeSend: function(request) {
-    //   request.setRequestHeader(/*"Access-Control-Allow-Origin", "*"*/"Access-Control-Allow-Headers", "x-requested-with" );
-    // },
-    // data : {
-    //   lat : userLatitude,
-    //   lon : userLongitude
-    // },
+    data : {
+      lat : userLatitude,
+      lon : userLongitude,
+      units : `imperial`,
+      "APPID" : myAPIKey
+    },
     method : 'GET',
     crossDomain : true,
     dataType: 'jsonp',
     success : function(data){
       console.log(data.main.temp);
-      alert("It worked");
-    }
+      // convert metric to imperial
 
+    }
   });
+}
+
+// convert metrics to imperial and vice versa
+function convertToImperial() {
+
+}
+
+function convertToMetric() {
+
 }
